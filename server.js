a17433e5598bd6a5b7fa7886d9da1b6bb11b21b1
Bryan P. Reyes <bryanpr17@gmail.com>
2023-10-11 08:17:45 +0800

    feat: setup monggose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require('./index');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT;
+
+const db = process.env.DATABASE_URL.replace('<password>', process.env.PASSWORD);
+
+mongoose.connect(db).then((con) => console.log(con.connection));
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
